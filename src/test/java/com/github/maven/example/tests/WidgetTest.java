a2ed754d65d9159fc1a66f702f19e1a28906c3e8
sree ram raja paladugula <rajapaladugula@gmail.com>
2016-12-30 15:47:35 -0600

    Update WidgetTest.java

--- src/test/java/com/github/maven/example/tests/WidgetTest.java
+++ src/test/java/com/github/maven/example/tests/WidgetTest.java
@@ -1,35 +1,8 @@
-package com.github.maven.example.tests;
+package hello;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import com.github.maven.example.Widget;
-
-import org.junit.Test;
-
-/**
- * Unit tests of {@link Widget} class
- */
-public class WidgetTest {
-
-	/**
-	 * Test default enable state of {@link Widget}
-	 */
-	@Test
-	public void defaultState() {
-		Widget widget = new Widget();
-		assertTrue(widget.isEnabled());
-	}
-
-	/**
-	 * Test enabling and disabling a {@link Widget}
-	 */
-	@Test
-	public void updateEnableState() {
-		Widget widget = new Widget();
-		widget.setEnabled(false);
-		assertFalse(widget.isEnabled());
-		widget.setEnabled(true);
-		assertTrue(widget.isEnabled());
-	}
+public class Helloworld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
 }
